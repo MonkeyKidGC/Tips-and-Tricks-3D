a3f52b39e0519be7aae1a1a2c0e4f6bd6567d921
Mrmatt1877 <monkeykidgc@gmail.com>
2021-01-01 17:32:00 -0500

    init

---
+++ Assets/Scripts/VelocityController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityController : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 ourVelocity;
+    private Rigidbody ourRigidbody;
+    void Start()
+    {
+        ourRigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToCenter();
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MoveTheCube();
+        }
+    }
+
+    private void MoveTheCube()
+    {
+        ourRigidbody.velocity = ourVelocity;
+    }
+    private void ReturnToCenter()
+    {
+        ourRigidbody.position = ourRigidbody.velocity = Vector3.zero;
+    }
+
+}
