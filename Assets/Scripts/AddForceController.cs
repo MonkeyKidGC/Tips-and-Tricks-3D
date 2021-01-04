1821fda6f51c624b3a639a7d4d80e5b5f0b90501
Mrmatt1877 <monkeykidgc@gmail.com>
2021-01-04 16:02:38 -0500

    AddForce

---
+++ Assets/Scripts/AddForceController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceController : MonoBehaviour
+{
+    private bool addTheForce;
+    private Rigidbody ourRigidbody;
+    [SerializeField]
+    private Vector3 ourVector;
+    [SerializeField]
+    private ForceMode ourForceMode;
+    [SerializeField]
+    private float thrust;
+
+    void Start()
+    {
+        ourRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToCenter();
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            addTheForce = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        if(addTheForce)
+        {
+            ourRigidbody.AddForce(ourVector * thrust, ourForceMode);
+            addTheForce = false;
+        }
+    }
+
+    private void ReturnToCenter()
+    {
+        ourRigidbody.position = ourRigidbody.velocity = Vector3.zero;
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is part of the Tips and Tricks 3D series hosted on https://www.MonkeyKidGC.
 
 Tutorials covered in this repo are
 Tips and Tricks: Unity Rigidbody Velocity - https://www.monkeykidgc.com/2021/01/unity-rigidbody-velocity.html?fbclid=IwAR2yvAO5B_OqUIU4wryPIhL_V_Uy_Rgpqrd7s6H6385wMFJJYcWyLDH13cI
+Tips and Tricks: Unity Rigidbody AddForce - https://www.monkeykidgc.com/2021/01/unity-rigidbody-addforce.html
