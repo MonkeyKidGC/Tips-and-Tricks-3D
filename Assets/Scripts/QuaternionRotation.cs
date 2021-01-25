2d0e60b2fdfb8a6a824fcbf6338371d47841530d
Mrmatt1877 <monkeykidgc@gmail.com>
2021-01-24 19:42:11 -0500

    new tutorials

---
+++ Assets/Scripts/QuaternionRotation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionRotation : MonoBehaviour
+{
+
+    [SerializeField] private GameObject rotationMimc;
+    [SerializeField] private bool isMimicRotation;
+    [SerializeField] private bool isRandomRotation;
+    [SerializeField] private Quaternion thatRotation;
+
+    private int timePassed = 2;
+
+    void Update()
+    {
+        if(isMimicRotation)
+        {
+            MimicRotation();
+        }
+        else if (isRandomRotation)
+        {
+            RandomRotation();
+        }
+        else 
+        {
+            transform.rotation = thatRotation;
+        }
+    }
+
+    private void MimicRotation()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation,rotationMimc.transform.rotation, 1f);
+    }
+
+    private void RandomRotation()
+    {
+        if(Time.time>=timePassed){
+            transform.rotation = Random.rotation;
+            timePassed += 2;
+        }
+    }
+}
